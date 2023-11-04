491a3b0119081352c563f5435987b5e5db4bc6b5
sieun <sieun960513@naver.com>
2023-11-04 17:21:34 +0900

    feat: 문자열 분리 클래스 생성

---
+++ src/main/java/stringCalculator/StringParser.java
@@ -0,0 +1,15 @@
+package stringCalculator;
+
+import java.util.List;
+
+public class StringParser {
+    private final String s;
+
+    public StringParser(String s) {
+        this.s = s;
+    }
+
+    public List<Ingredient> doParse(){
+
+    }
+}
