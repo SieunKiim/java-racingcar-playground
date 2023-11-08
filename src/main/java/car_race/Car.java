de87bb0b4c4a0ffbcb510f6358ce989bf009ad68
sieun <sieun960513@naver.com>
2023-11-09 01:10:16 +0900

    feat: 자동차 객체 생성

---
+++ src/main/java/car_race/Car.java
@@ -0,0 +1,9 @@
+package car_race;
+
+public class Car {
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/car_race/CarTest.java
@@ -0,0 +1,10 @@
+package car_race;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void 자동차_생성(){
+        Car car = new Car("name");
+    }
+}
