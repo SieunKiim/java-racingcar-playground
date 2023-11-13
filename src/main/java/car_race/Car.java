115481f74d2c41ba8703f56fd3592205d1616e0f
sieun <sieun960513@naver.com>
2023-11-13 21:12:36 +0900

    feat: 자동차 객체 생성

--- src/main/java/car_race/Car.java
+++ src/main/java/car_race/Car.java
@@ -4,6 +4,13 @@ public class Car {
     private final String name;
 
     public Car(String name) {
+        if (!nameValidation(name)) {
+            throw new RuntimeException("5글자 초과 이름");
+        }
         this.name = name;
+    }
+
+    private boolean nameValidation(String name){
+        return name.length() <= 5;
     }
 }
--- src/test/java/car_race/CarTest.java
+++ src/test/java/car_race/CarTest.java
@@ -1,10 +1,29 @@
 package car_race;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+
+    Car car;
+
+    @BeforeEach
+    void init(){
+        car = new Car("init");
+    }
     @Test
     void 자동차_생성(){
-        Car car = new Car("name");
+        Car carInTest = new Car("name");
+        assertThat(carInTest).isNotNull();
+    }
+
+    @Test
+    void 자동차_생성_오류(){
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> new Car("moreThanFive"));
+        assertThat(exception.getMessage()).isEqualTo("5글자 초과 이름");
     }
 }
